239b104224f787a21875b3d36e527fa8af27160f
Recep Öztürk <recep.fed@gmail.com>
2021-05-12 14:42:54 +0300

    About page layout is structured

---
+++ pages/blog.js
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div>
+      <h3>BLOG</h3>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+import About from "./about";
 
 export default function Home() {
   return (
@@ -10,19 +11,26 @@ export default function Home() {
         <meta name="description" content="Coming soon.." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <header className={`${styles.header}`}>
+        <div>
+          <a>TR</a>
+          <a>EN</a>
+          <a>DE</a>
+        </div>
+      </header>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome !
-        </h1>
-        
-
-      
+        <div className={styles.navcol}>
+          <div>ABOUT</div>
+          <div>BLOG</div>
+          <div>CONTACT</div>
+        </div>
+        <div className={styles.content}>
+          {/* <h3>CONTENT</h3> */}
+          <About />
+        </div>
       </main>
 
-      <footer className={styles.footer}>
-        
-      </footer>
+      <footer className={styles.footer}>Here comes the footer</footer>
     </div>
-  )
+  );
 }
