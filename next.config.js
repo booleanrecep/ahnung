f618cee130eba2731b9e69fcd86bfe2bf0e3ed16
Recep Öztürk <recep.fed@gmail.com>
2021-05-27 19:59:59 +0300

    next.config.js env variable edited

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
+const {MONGODB_URI,MONGODB_DB} = process.env
+
 module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/about',
-          permanent: true,
-        },
-      ]
-    }
-  }
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/about",
+        permanent: true,
+      },
+    ];
+  },
+  env: {
+    local: {
+      MONGODB_URI: MONGODB_URI,
+      MONGODB_DB: MONGODB_DB,
+    },
+  },
+};
