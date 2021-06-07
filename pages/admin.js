53a3db2ae4a82748c3ae111721bf6c0586d89b17
Recep Öztürk <recep.fed@gmail.com>
2021-06-08 00:56:58 +0300

    Adding transfrom to Navcol

--- pages/admin.js
+++ pages/admin.js
@@ -1,6 +1,10 @@
 import React from "react";
+import {useRouter} from "next/router"
+
 import styles from "../styles/pages/admin.module.scss";
 const Admin = () => {
+  const router = useRouter()
+
   const [state, setState] = React.useState({ title: "", text: "" });
   const handleChange = (e) => {
     e.preventDefault();
@@ -14,32 +18,44 @@ const Admin = () => {
     e.preventDefault();
     console.log(state);
   };
+  const [min, setMin] = React.useState(1);
+  React.useEffect(() => {
+    const id = setTimeout(() => setMin(0), 500);
+    return () => {
+      setMin(1);
+      clearTimeout(id);
+    };
+  }, [router.query.lang]);
 
   return (
     <>
-      <form className={styles.articleForm}>
-        <textarea
-          name="title"
-          onChange={handleChange}
-          className={styles.title}
-          type="text"
-          maxLength="70"
-          placeholder="Title..."
-        />
-        <textarea
-          name="text"
-          onChange={handleChange}
-          rows="30"
-          className={styles.textarea}
-          placeholder="Text..."
-        />
-        <input
-          className={styles.submit}
-          type="button"
-          onClick={handleSubmit}
-          value="Submit"
-        />
-      </form>
+      {min == 1 ? (
+        <div className="loader" />
+      ) : (
+        <form className={styles.articleForm}>
+          <textarea
+            name="title"
+            onChange={handleChange}
+            className={styles.title}
+            type="text"
+            maxLength="70"
+            placeholder="Title..."
+          />
+          <textarea
+            name="text"
+            onChange={handleChange}
+            rows="30"
+            className={styles.textarea}
+            placeholder="Text..."
+          />
+          <input
+            className={styles.submit}
+            type="button"
+            onClick={handleSubmit}
+            value="Submit"
+          />
+        </form>
+      )}
     </>
   );
 };
