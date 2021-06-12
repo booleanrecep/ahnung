49b844d6d8fc0bed8a2c2de075d5eda2299fd876
Recep Öztürk <recep.fed@gmail.com>
2021-06-12 11:58:13 +0300

    Special chars in query string & style prettified

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,23 @@
 import React from "react";
 import styles from "../styles/components/Header.module.scss";
-import Link from "next/link"
-import {useRouter} from "next/router"
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export const Header = (props) => {
-  const router=useRouter()
+  const router = useRouter();
   const ref = React.createRef();
-  const objStyle1 = {
+  const objStyleBegin = {
     transform: "rotate(90deg)",
     margin: "50px -30px 0 0",
   };
-  const objStyle2 = {
+  const objStyleEnd = {
     transform: "rotate(0deg)",
     margin: "10px",
   };
-  const childStyle1 = {
+  const childStyleBegin = {
     transform: "rotate(-90deg)",
   };
-  const childStyle2 = {
+  const childStyleEnd = {
     transform: "rotate(0deg)",
   };
   const objStyles = (obj, sty) => {
@@ -28,12 +29,11 @@ export const Header = (props) => {
       .map((chl) => Object.assign(chl.style, sty));
   };
   const handlePosition = React.useCallback(() => {
-   
     window.pageYOffset > 50 && window.innerWidth > 768
-      ? (objStyles(ref.current, objStyle1),
-        chldStyles(ref.current.children, childStyle1))
-      : (objStyles(ref.current, objStyle2),
-        chldStyles(ref.current.children, childStyle2));
+      ? (objStyles(ref.current, objStyleBegin),
+        chldStyles(ref.current.children, childStyleBegin))
+      : (objStyles(ref.current, objStyleEnd),
+        chldStyles(ref.current.children, childStyleEnd));
   }, [ref]);
 
   React.useEffect(() => {
@@ -43,11 +43,11 @@ export const Header = (props) => {
     };
   }, [ref]);
   return (
-    <header className={`${styles.header}`} >
-      <div id="lang" ref={ref} style={{display:props.display}}>
-       <Link href={router.pathname+"?lang=tr"} >TR</Link>
-        <Link href={router.pathname+"?lang=en"} >EN</Link>
-        <Link href={router.pathname+"?lang=de"}>DE</Link>
+    <header className={`${styles.header}`}>
+      <div id="lang" ref={ref} style={{ display: props.display }}>
+        <Link href={router.pathname + "?lang=tr"}>TR</Link>
+        <Link href={router.pathname + "?lang=en"}>EN</Link>
+        <Link href={router.pathname + "?lang=de"}>DE</Link>
       </div>
     </header>
   );
--- components/Layout.js
+++ components/Layout.js
@@ -4,17 +4,16 @@ import Head from "next/head";
 import React from "react";
 
 export const Layout = (props) => {
-  const [style,setStyle]=React.useState({display:"flex"})
+  const [style, setStyle] = React.useState({ display: "flex" });
   return (
     <>
-    
       <Head>
         <title>AHNUNG</title>
         <meta name="description" content="Coming soon.." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="container">
-        <Header display={style.display}/>
+        <Header display={style.display} />
         <main className="main">
           <Navcol handleDisplay={setStyle} />
           <div className="content">{props.children}</div>
