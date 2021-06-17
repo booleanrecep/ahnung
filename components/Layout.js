44117ad40114b46c09dbe6a0e320c54b69d57ac8
Recep Öztürk <recep.fed@gmail.com>
2021-06-17 13:33:25 +0300

    site under construction animation is added

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,8 @@ import React from "react";
 import Head from "next/head";
 import { Header } from "./Header";
 import { Navcol } from "./Navcol";
-
 export const Layout = (props) => {
+  const size = 30;
   const [style, setStyle] = React.useState({ display: "flex" });
   return (
     <>
@@ -13,6 +13,44 @@ export const Layout = (props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="container">
+        <div
+          style={{ position: "fixed", left: 10, width: "60px" }}
+          title="The site is still under construction."
+        >
+          <img
+            src="/static/gear-2.png"
+            style={{
+              animation: "spin 7s linear infinite",
+              position: "fixed",
+              width: `${size}px`,
+              height: `${size}px`,
+              marginLeft: `${size - 28}px`,
+              marginTop: `${size - 25}px`,
+            }}
+          />
+          <img
+            src="/static/gear-2.png"
+            style={{
+              animation: "spin 7s linear infinite",
+              position: "fixed",
+              width: `${size - 6}px`,
+              height: `${size - 6}px`,
+              marginLeft: `${size + 11}px`,
+              marginTop: `${size - 7}px`,
+            }}
+          />
+          <img
+            src="/static/gear-2.png"
+            style={{
+              animation: "spin 7s reverse  linear infinite",
+              position: "fixed",
+              width: `${size - 3}px`,
+              height: `${size - 3}px`,
+              marginLeft: `${size - 13}px`,
+              marginTop: `${size - 4}px`,
+            }}
+          />
+        </div>
         <Header display={style.display} />
         <main className="main">
           <Navcol handleDisplay={setStyle} />
